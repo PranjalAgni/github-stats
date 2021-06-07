84a77ee352bb9508c58a862f3038e3ba94a7fe28
PranjalAgni <pranjal.agnihotri6@gmail.com>
2021-06-07 17:25:27 +0530

    :rainbow: FE setup

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootswatch/dist/lux/bootstrap.min.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
